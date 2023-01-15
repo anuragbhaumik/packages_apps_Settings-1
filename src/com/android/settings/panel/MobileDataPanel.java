Havoc-OS/android_packages_apps_Settings

Settings: panels: Improvements
- Adapt bottom sheet corner radius to system
- Changed Wifi slice string to match Settings
- Changed Mobile data slice title to match carrier (like in Settings)

Change-Id: I05b5751f140944812fb962d4dea3bfd0cff3b81d

src/com/android/settings/panel/MobileDataPanel.java
@@ -20,6 +20,8 @@ package com.android.settings.panel;
 import android.app.settings.SettingsEnums;
 import android.content.Context;
 import android.content.Intent;
+import android.telephony.SubscriptionInfo;
+import android.telephony.SubscriptionManager;
 import android.net.Uri;
 
 import com.android.settings.R;
@@ -37,6 +39,7 @@ import java.util.List;
 public class MobileDataPanel implements PanelContent {
 
     private final Context mContext;
+    private final SubscriptionManager mSubscriptionManager;
 
     public static MobileDataPanel create(Context context) {
         return new MobileDataPanel(context);
@@ -44,11 +47,12 @@ public class MobileDataPanel implements PanelContent {
 
     private MobileDataPanel(Context context) {
         mContext = context.getApplicationContext();
+        mSubscriptionManager = context.getSystemService(SubscriptionManager.class);
     }
 
     @Override
     public CharSequence getTitle() {
-        return mContext.getText(R.string.cellular_data_title);
+        return getSummary();
     }
 
     @Override
@@ -77,5 +81,15 @@ public class MobileDataPanel implements PanelContent {
     @Override
     public int getMetricsCategory() {
         return SettingsEnums.PANEL_MOBILE_DATA;
+    }
+
+    private CharSequence getSummary() {
+        final SubscriptionInfo defaultSubscription =
+                mSubscriptionManager.getDefaultDataSubscriptionInfo();
+        if (defaultSubscription == null) {
+            return mContext.getText(R.string.cellular_data_title).toString();
+        }
+
+        return defaultSubscription.getDisplayName();
     }
 }

anuragbhaumik/packages_apps_Settings-1

Change-Id: I9c34c2cd6922864a19ddc1aa760c604498650199

src/com/android/settings/panel/MobileDataPanel.java
@@ -20,6 +20,8 @@ package com.android.settings.panel;
 import android.app.settings.SettingsEnums;
 import android.content.Context;
 import android.content.Intent;
+import android.telephony.SubscriptionInfo;
+import android.telephony.SubscriptionManager;
 import android.net.Uri;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
@@ -39,6 +41,7 @@ import java.util.List;
 public class MobileDataPanel implements PanelContent {
 
     private final Context mContext;
+    private final SubscriptionManager mSubscriptionManager;
 
     public static MobileDataPanel create(Context context) {
         return new MobileDataPanel(context);
@@ -46,11 +49,12 @@ public class MobileDataPanel implements PanelContent {
 
     private MobileDataPanel(Context context) {
         mContext = context.getApplicationContext();
+        mSubscriptionManager = context.getSystemService(SubscriptionManager.class);
     }
 
     @Override
     public CharSequence getTitle() {
-        return mContext.getText(R.string.cellular_data_title);
+        return getSummary();
     }
 
     @Override
@@ -64,7 +68,7 @@ public class MobileDataPanel implements PanelContent {
     @Override
     public Intent getSeeMoreIntent() {
         final String screenTitle =
-                mContext.getText(R.string.cellular_data_title).toString();
+                mContext.getText(R.string.mobile_data_settings_title).toString();
         final Intent intent = SliceBuilderUtils.buildSearchResultPageIntent(mContext,
                 NetworkDashboardFragment.class.getName(),
                 null /* key */,
@@ -79,5 +83,15 @@ public class MobileDataPanel implements PanelContent {
     @Override
     public int getMetricsCategory() {
         return MetricsEvent.PANEL_MOBILE_DATA;
+    }
+
+    private CharSequence getSummary() {
+        final SubscriptionInfo defaultSubscription =
+                mSubscriptionManager.getDefaultDataSubscriptionInfo();
+        if (defaultSubscription == null) {
+            return mContext.getText(R.string.mobile_data_settings_title).toString();
+        }
+
+        return defaultSubscription.getDisplayName();
     }
 }